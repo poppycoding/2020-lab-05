25f75ed8de1af79684dcde428a6eda443006c1e5
xlasers <657344389@qq.com>
2018-11-26 15:17:12 +0800

    :sparkles: add shell cmd for netstat

--- linux/kill.sh
+++ linux/kill.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+
+
+############ 端口-进程互相查询命令
+netstat -nap | grep 8080 | grep LISTEN
+netstat -nap | grep 10425 | grep LISTEN
+
+
+
 ############ kill命令
 # kill process: dms.jar (v反转)
 ps -ef | grep dms.jar | grep -v grep | awk '{print $2}' | xargs kill -9
