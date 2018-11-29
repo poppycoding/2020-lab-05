44d10c92e696d41ed20faa2a984eb5eb2bde17d3
xlasers <657344389@qq.com>
2018-11-29 14:19:23 +0800

    :sparkles: add pwd relation for neo4j

--- data-manager/neo4j/cmd.sh
+++ data-manager/neo4j/cmd.sh
@@ -6,3 +6,10 @@ neo4j.bat status | stop | start | restart
 
 #linuxs: neo4j cmd
 ./neo4j status | stop | start | restart
+
+
+#changePwd 没有忘记密码,修改密码
+:server change-password
+
+#deletePwd 忘记密码
+删除data/dbms/auth文件--重新启动
