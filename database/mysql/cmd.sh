30898e4b27c009f60f76df1f53160605bbd58c02
xlasers <657344389@qq.com>
2018-12-18 15:08:53 +0800

    :recycle: refactor sql for count

---
+++ database/mysql/cmd.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#mysql手写ping程序获取连接时,报错时区问题,可以临时设置数据库时区命令(或者代码里设置url拼接加上时区&serverTimezone=GMT%2B8)
+#查看
+show variables like "%time_zone%";
+#修改
+set global time_zone = '+8:00';
+#刷新
+flush privileges;
+
+
--- database/oracle/sys.sql
+++ database/oracle/sys.sql
@@ -13,15 +13,26 @@ grant read,write on directory exp_dir to public;
 
 
 
---3.查看数据库[表格-列]数量
+--3.查看数据库[表格-视图-列(table+view)]数量
 --user_tables(当前登陆)
 select count(*) from user_tables;
+select count(*) from user_views;
 select count(column_name) from user_tab_columns [where table_name = 'TEST'];
 --dba_tables(dba权限)
 select count(*) from dba_tables where owner='TEST';
+select count(*) from dba_views where owner = 'TEST'
 select count(column_name) from dba_tab_columns [where owner='TEST'];
 --all_tables(dba权限)
 select count(*) from all_tables where owner='TEST';
+select count(*) from all_views where owner = 'TEST';
 select count(column_name) from all_tab_columns [where owner='TEST'];
 
 
+
+--4.查看schema下所有: 数据库+表格+视图+列数量
+SELECT
+t.table_num, v.view_num, c.column_num, (t.table_num + v.view_num + c.column_num + 1) count_all
+FROM
+(select count(*) table_num from dba_tables where owner='HC_XHCT') t,
+(select count(*) view_num from dba_views where owner = 'HC_XHCT') v,
+(select count(column_name) column_num from dba_tab_columns where owner='HC_XHCT') c
