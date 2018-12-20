d506d7d5db0244938d3e153c0407bbb8f4121f99
xlasers <657344389@qq.com>
2018-12-20 19:37:21 +0800

    :sparkles: add some linux shell for system

--- linux/sys.sh
+++ linux/sys.sh
@@ -69,6 +69,27 @@ cat /etc/redhat-release
 lsb_release -a
 
 
+# 查看linux配置内核
+# 总核数 = 物理CPU个数 X 每颗物理CPU的核数
+# 总逻辑CPU数 = 物理CPU个数 X 每颗物理CPU的核数 X 超线程数
+#一般情况下，逻辑cpu=物理CPU个数×每颗核数，如果不相等的话，则表示服务器的CPU支持超线程技术
+
+# 查看物理CPU个数
+cat /proc/cpuinfo| grep "physical id"| sort| uniq| wc -l
+
+# 查看每个物理CPU中core的个数(即核数)
+cat /proc/cpuinfo| grep "cpu cores"| uniq
+
+# 查看逻辑CPU的个数
+cat /proc/cpuinfo| grep "processor"| wc -l
+
+# 查看CPU信息（型号）
+cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c
+
+#查看内 存信息
+cat /proc/meminfo
+
+
 
 # 带宽信息查看
 sudo ethtool eth1
