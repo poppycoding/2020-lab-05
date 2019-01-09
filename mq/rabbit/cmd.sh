7a098c13ab93387286f18abfab8943e305394b17
xlasers <657344389@qq.com>
2019-01-09 14:43:43 +0800

    :sparkles: complete the install rabbit steps

--- mq/rabbit/cmd.sh
+++ mq/rabbit/cmd.sh
@@ -1,66 +1,11 @@
 #!/usr/bin/env bash
-echo 192.168.200.156 mq > /etc/hosts
-echo rabbitmq > /etc/hostname
-export HOSTNAME=mq
-
-
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
-::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-
-
-
-[rabbitmq-erlang]
-name=rabbitmq-erlang
-baseurl=https://dl.bintray.com/rabbitmq/rpm/erlang/20/el/7
-gpgcheck=1
-gpgkey=https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc
-repo_gpgcheck=0
-enabled=1
-
-yum install -y erlang
-
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-
-
-
-
-[{rabbit, [{loopback_users, []}]}].
-
-
-
-
-
-
-
-
-
-
+# 待定
 rpm -Uvh https://dl.bintray.com/rabbitmq/all/rabbitmq-server/3.7.3/rabbitmq-server-3.7.3-1.el7.noarch.rpm
 
 
-
-
-rabbitmqctl add_user admin admin
-rabbitmqctl set_user_tags admin administrator
-rabbitmqctl  set_permissions -p "/" admin '.*' '.*' '.*'
-rabbitmqctl list_user_permissions admin
-
-
-
-
-
-
-
-
-
-
-
-
--------
-
+# 准erlang环境
+# 添加
 vim /etc/yum.repos.d/rabbitmq-erlang.repo
-
-
 [rabbitmq-erlang]
 name=rabbitmq-erlang
 baseurl=https://dl.bintray.com/rabbitmq/rpm/erlang/20/el/7
@@ -68,25 +13,42 @@ gpgcheck=1
 gpgkey=https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc
 repo_gpgcheck=0
 enabled=1
+# 安装
+yum install -y erlang
 
 
-
-
-
-rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-
+# 准备rabbitmq环境
 wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-
 yum install rabbitmq-server-3.6.10-1.el7.noarch.rpm
 
 
+# 添加配置文件内容(允许guest访问): [{rabbit, [{loopback_users, []}]}].
+vi /etc/rabbitmq/rabbitmq.config
+
+
+# 启动/关闭/重启
 systemctl start rabbitmq-server
 systemctl status rabbitmq-server
 systemctl restart rabbitmq-server
+
+
 #设置为开机启动
 systemctl enable rabbitmq-server
 
 
+# 开启web页面: 15672
+rabbitmq-plugins enable rabbitmq_management
 
-vi /etc/rabbitmq/rabbitmq.config
-loopback_users = none
+
+# 开放,15672,5672端口
+firewall-cmd --zone=public --add-port=15672/tcp --permanent
+firewall-cmd --zone=public --add-port=5672/tcp --permanent
+firewall-cmd --reload
+
+
+# 默认guest/guest访问(只能访问localhost,上面配置loopback_users即可)
+# 添加 admin/admin用户
+rabbitmqctl add_user admin admin
+rabbitmqctl set_user_tags admin administrator
+rabbitmqctl  set_permissions -p "/" admin '.*' '.*' '.*'
+rabbitmqctl list_user_permissions admin
