e9869178f39d2a31920df267f13109e624b5d2cd
xlasers <657344389@qq.com>
2019-02-28 16:41:35 +0800

    :recycle: refactor code for java

--- bravo/Status.java
+++ bravo/Status.java
@@ -47,8 +47,7 @@ public enum Status {
     /**
      * json参数解析异常{@link HttpMessageNotReadableException}
      *
-     * <p>eg: long类型字段,用integer接收,导致字段长度越界问题</p>
-     * <p>eg: lombok插件用@Builder注解,造成实体类没有默认无参构造函数,导致json传参反序列化失败,禁止用builder注解!</p>
+     * <p>eg: long类型字段,用integer接收</p>
      */
     PARSE_JSON_ERROR(1000, "JSON参数解析异常!"),
 
@@ -75,7 +74,37 @@ public enum Status {
     /**
      * 绑定失败
      */
-    BIND_FAILED(3001, "失败! 用户已经绑定职工号发生错误,稍后重试!");
+    BIND_FAILED(3001, "jdbc异常! 用户绑定职工号发生错误,请联系管理员,稍后重试!"),
+
+    /**
+     * 重复发绑定
+     */
+    BIND_REPEATED(3002, "绑定异常! 该职工号对应用户已经绑定!"),
+
+    /**
+     * 保存报告失败
+     */
+    SAVE_REPORT_FAILED(4000, "jdbc异常! 保存报告发生错误,请联系管理员,稍后重试!"),
+
+    /**
+     * 前端接口调错,重复保存报告,后续应该更新报告
+     */
+    SAVE_REPORT_REPEATED(4001, "报告提交异常! 今天已经提交,后续进行更操作,前端查看接口是否调用错误!"),
+
+    /**
+     * 报告类型错误
+     */
+    REPORT_TYPE_ERROR(4002, "错误! 总结类型-1:日报, 2:周报, 3:月度总结, 4:年度总结"),
+
+    /**
+     * 获取报告详情错误,该id不存在
+     */
+    REPORT_DETAIL_ERROR(4003, "获取总结详情异常! 该id下无数据,疑似手动误操作数据库导致!"),
+
+    /**
+     * 更新报告失败
+     */
+    UPDATE_REPORT_FAILED(4004, "异常! 更新报告错误,对应该id的报告不存在,疑似手动误操作数据库导致!");
 
     /**
      * 状态码
--- bravo/exception/HospitalExceptionHandler.java
+++ bravo/exception/HospitalExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,31 +39,27 @@ public class HospitalExceptionHandler {
     @ResponseBody
     public ApiResponse handlerException(Exception e) {
         if (e instanceof NoHandlerFoundException) {
-
             log.error("【全局异常拦截】NoHandlerFoundException: 请求方法 {}, 请求路径 {}", ((NoHandlerFoundException) e).getRequestURL(), ((NoHandlerFoundException) e).getHttpMethod());
             return ApiResponse.ofStatus(Status.REQUEST_NOT_FOUND);
         } else if (e instanceof MethodArgumentNotValidException) {
-
             log.error("【全局异常拦截】MethodArgumentNotValidException", e.getMessage());
             return ApiResponse.of(Status.BAD_REQUEST.getCode(), ((MethodArgumentNotValidException) e).getBindingResult().getAllErrors().get(0).getDefaultMessage(), null);
         } else if (e instanceof MethodArgumentTypeMismatchException) {
-
             log.error("【全局异常拦截】MethodArgumentTypeMismatchException: 参数名 {}, 异常信息 {}", ((MethodArgumentTypeMismatchException) e).getName(), e.getMessage());
-            return ApiResponse.ofStatus(Status.PARAM_NOT_MATCH);
+            return ApiResponse.ofStatus(Status.PARAM_NOT_MATCH, ((MethodArgumentTypeMismatchException) e).getName());
         } else if (e instanceof HttpMessageNotReadableException) {
-
             log.error("【全局异常拦截】HttpMessageNotReadableException: 错误信息 {}", e.getMessage());
             return ApiResponse.ofStatus(Status.PARSE_JSON_ERROR);
         } else if (e instanceof WxErrorException) {
-
             log.error("【全局异常拦截】WxErrorException: 错误信息 {}", e.getMessage());
-            return ApiResponse.of(((WxErrorException) e).getError().getErrorCode(),((WxErrorException) e).getError().getErrorMsg(),null);
+            return ApiResponse.of(((WxErrorException) e).getError().getErrorCode(), ((WxErrorException) e).getError().getErrorMsg(), null);
+        } else if (e instanceof MissingServletRequestParameterException) {
+            log.error("【全局异常拦截】MissingServletRequestParameterException: 错误信息 {}", e.getMessage());
+            return ApiResponse.ofStatus(Status.PARAM_NOT_NULL, ((MissingServletRequestParameterException) e).getParameterName());
         } else if (e instanceof HospitalException) {
-
             log.error("【全局异常拦截】HospitalException: 状态码 {}, 异常信息 {}", ((HospitalException) e).getCode(), e.getMessage());
             return ApiResponse.ofException((HospitalException) e);
         }
-
         log.error("【全局异常拦截】: 异常信息 {} ", e.getMessage());
         return ApiResponse.ofStatus(Status.UNKNOWN_ERROR);
     }
