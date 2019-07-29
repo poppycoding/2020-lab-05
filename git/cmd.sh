d2897f3d918b3c12d5cf0437e1a9d16d81c2cb21
elijahdu <Elijah_T_Du@manulife.com>
2019-07-29 17:05:59 +0800

    feat: add cmd for git modify commit msg

--- git/cmd.sh
+++ git/cmd.sh
@@ -16,8 +16,8 @@ git config user.name
 git config user.email
 
 # 设置/修改用户信息
-git config --global user.name "xlasers"
-git config --global user.email "657344389@qq.com"
+git config --global user.name "xlaser"
+git config --global user.email "xlaser@gmail.com"
 
 
 
@@ -29,4 +29,17 @@ git update
 git update-git-for-windows
 
 
+# git修改提交信息
+
+# 最近5次简要信息 (详细git log -5)
+git log --oneline -5
+
+# 选择要修改的commitID下面一个!! 会展示选择的commitID之后所有的提交记录
+git rebase -i commitID
+
+# 确认要修改的commitID将pick改为reword
+pick -> reword
+
+# 之后再push
+push
 
